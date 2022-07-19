5f1e39f0c392f65b8abf6856cb020c15ea708d02
ZohaibCyber <104460619+ZohaibCyber@users.noreply.github.com>
2022-07-19 12:01:28 +0500

    Custom map functions

--- ArrayPrototypeCustomMapMethod.js
+++ ArrayPrototypeCustomMapMethod.js
@@ -2,19 +2,21 @@
 Write a program to make polyfill for map method
 in javascript
 
-Array Prototype method
+Array Prototype method - Array.mymap((item)=>{ item * 5 })
 
 */
 
 Array.prototype.mymap = function (cb) {
-let temp = [];
-for (let i = 0; i < this.length; i++) {
-temp.push(cb(this[i]));
-}
-return temp;
+    let temp = [];
+    for (let i = 0; i < this.length; i++) {
+        temp.push(cb(this[i]));
+    }
+    return temp;
 };
+
 const arr = [2, 3, 4, 5];
 const result = arr.mymap((num) => {
-return num * 5;
+    return num * 5;
 });
+
 console.log("result", result);
