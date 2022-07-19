ce3f51051ca8c512b844405eff67810ee07b5ca0
ZohaibCyber <104460619+ZohaibCyber@users.noreply.github.com>
2022-07-19 12:21:43 +0500

    Sort Algorithms

--- sortArrayInDecending.js
+++ sortArrayInDecending.js
@@ -17,3 +17,22 @@ console.log("Elements of array sorted in dcending order:");
 for (let i = 0; i < arr.length; i++) {
     console.log(arr[i]);
 }
+
+
+// Sesond Approch
+const arr2 = [3,2,5,4,1,0];
+
+for (let i = 0; i < arr2.length; i++) {
+    for (let j = i+1; j < arr2.length; j++) {
+        if(arr2[i] < arr2[j]) {
+            [arr2[i], arr2[j]] = [arr2[j], arr2[i]];
+             
+        }
+    }
+}
+
+console.log("Elements of array sorted in dcending order:");
+
+for (let i = 0; i < arr2.length; i++) {
+    console.log(arr2[i]);
+}
