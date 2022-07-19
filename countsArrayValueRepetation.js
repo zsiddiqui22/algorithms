3300c1e707b802d454a46889ced2d26d3765d874
ZohaibCyber <104460619+ZohaibCyber@users.noreply.github.com>
2022-07-19 12:01:44 +0500

    updated

--- countsArrayValueRepetation.js
+++ countsArrayValueRepetation.js
@@ -5,9 +5,9 @@ const arr = [1,1,2,3,1,4]
 const count = {};
     for (const element of arr) {
     if (count[element]) {
-    count[element] += 1;
+        count[element] += 1;
     } else {
-    count[element] = 1;
+        count[element] = 1;
     }
 }
 
